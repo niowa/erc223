1e6a8a055e9fe9572fc2da54be0dacbedf00f62f
niowa <andres7480311@mail.ru>
2018-08-14 10:40:48 +0300

    refactored migration

--- migrations/2_deploy_token_and_crowdsale.js
+++ migrations/2_deploy_token_and_crowdsale.js
@@ -3,17 +3,11 @@ const Crowdsale = artifacts.require('Crowdsale');
 const getConfig = require('../config.js');
 
 module.exports = function (deployer, network, accounts) {
-  let deployedToken;
-  const config = getConfig(accounts);
+  deployer.then(async () => {
+    const config = getConfig(accounts);
 
-  return deployer.then(() => (
-    deployer.deploy(Token, 'MyCHIP', 'CHIP', config.tokenDecimalPlaces, config.transferLockPeriod, config.options)
-  ))
-    .then((token) => {
-      deployedToken = token;
-      return deployer.deploy(Crowdsale, token.address, config.tokenPrice, config.tokenRate, config.options)
-    })
-    .then((deployedCrowdsale) => (
-      deployedToken.setTokenGenerator(deployedCrowdsale.address, config.options)
-    ))
+    const token = await deployer.deploy(Token, 'MyCHIP', 'CHIP', config.tokenDecimalPlaces, config.transferLockPeriod, config.options);
+    const crowdsale = await deployer.deploy(Crowdsale, token.address, config.tokenPrice, config.tokenRate, config.options);
+    await token.setTokenGenerator(crowdsale.address, config.options)
+  });
 };
